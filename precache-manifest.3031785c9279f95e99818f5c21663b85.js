7118881cb12a3a41c313fa147ae9eaca4d22a719
Taylor Plante <taylorplante99@gmail.com>
2018-11-21 15:06:07 +0100

    Updates

---
+++ precache-manifest.3031785c9279f95e99818f5c21663b85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d01e35b4628fe00b4c76",
+    "url": "/sorting-ui-4/static/js/runtime~main.d01e35b4.js"
+  },
+  {
+    "revision": "427a06eebbfafcf326ac",
+    "url": "/sorting-ui-4/static/js/main.427a06ee.chunk.js"
+  },
+  {
+    "revision": "9f9d3b707a9f19c61e82",
+    "url": "/sorting-ui-4/static/js/1.9f9d3b70.chunk.js"
+  },
+  {
+    "revision": "427a06eebbfafcf326ac",
+    "url": "/sorting-ui-4/static/css/main.b59dc112.chunk.css"
+  },
+  {
+    "revision": "01dfffec7e23fdd1cf65e618fb9bb2e7",
+    "url": "/sorting-ui-4/index.html"
+  }
+];
